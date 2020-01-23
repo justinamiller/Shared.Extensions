a7d385fec894661b950eae82262c64585fc429bd
Justin Miller <justinamiller@gmail.com>
2020-01-23 07:58:10 -0700

    adding unit tests

---
+++ tests/Dotnet.Extensions.TestFramework/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Dotnet.Extensions.TestFramework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Dotnet.Extensions.TestFramework")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b5045188-75fd-4de8-9f72-2e0c9fd87d58")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ tests/Dotnet.Extensions.TestFramework/StringExtensionsTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dotnet.Extensions.TestFramework
+{
+    [TestClass]
+    public class StringExtensionsTest
+    {
+        [TestMethod]
+        public void IsNullOrEmptyTest()
+        {
+            Assert.IsTrue("".IsNullOrEmpty());
+            Assert.IsTrue(((string)null).IsNullOrEmpty());
+            Assert.IsFalse("Hi".IsNullOrEmpty());
+        }
+    }
+}
