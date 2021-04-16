d936fcd636c7d6e23387026a47b6feee91ec390a
Justin Miller <justamiller@msn.com>
2021-04-16 14:51:32 -0700

    adding some reflection helper

---
+++ src/ReflectionExtensions.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Shared.Extensions
+{
+    public static class ReflectionExtensions
+    {
+        private const BindingFlags _bindingFlags = BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance;
+        private static readonly ConcurrentDictionary<Type, PropertyInfo[]> s_TypeCache = new ConcurrentDictionary<Type, PropertyInfo[]>();
+        private static PropertyInfo[] GetProperties<T>(this T source)
+        {
+            return source.GetType().GetTypeProperties();
+        }
+
+        private static PropertyInfo[] GetTypeProperties(this Type type)
+        {
+            PropertyInfo[] properties;
+            //check if we know of this type?
+            if (!s_TypeCache.TryGetValue(type, out properties))
+            {
+                //nope new type needs to reflect on the object.
+                properties = type.GetProperties(_bindingFlags);
+                //do not cache generic types
+                if (!type.IsGenericType)
+                {
+                    //add to cache; improve perf by only reflect once.
+                    s_TypeCache.TryAdd(type, properties);
+                }
+            }
+
+            return properties;
+        }
+
+        private static string GetTypeFriendlyName(this Type type)
+        {
+            StringBuilder sb = StringBuilderPool.Get();
+            sb.Append(type.Namespace);
+            sb.Append(".");
+            sb.Append(type.Name);
+
+            if (type.IsGenericType)
+            {
+                //generic object type extract the details on types
+                Type[] arguments = type.GenericTypeArguments;
+                sb.Append("[");
+
+                for (int i = 0; i < arguments.Length; i++)
+                {
+                    if (i > 0)
+                    {
+                        sb.Append(",");
+                    }
+                    Type argument = arguments[i];
+                    sb.Append("[");
+                    sb.Append(argument.Namespace);
+                    sb.Append(".");
+                    sb.Append(argument.Name);
+                    sb.Append("]");
+                }//end for
+
+                sb.Append("]");
+            }
+
+            return sb.GetStringAndRelease();
+        }
+
+        /// <summary>
+        /// Transform an object's root properties to IDictionary<string, object> will reflect only on non-static public properties
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static IDictionary<string, object> AsDictionary<T>(this T source) where T : class
+        {
+            if (source == null)
+            {
+                //is null
+                return new Dictionary<string, object>();
+            }
+
+            if (source is IDictionary<string, object> castDictionary)
+            {
+                return castDictionary;
+            }
+
+            PropertyInfo[] properties = source.GetProperties();
+            int length = properties.Length;
+            var dictionary = new Dictionary<string, object>(length)
+            {
+                //Add type name as field.
+                { "___type", typeof(T).GetTypeFriendlyName() }
+            };
+            PropertyInfo property;
+            //reflection on object
+            for (var i = 0; i < length; i++)
+            {
+                property = properties[i];
+                if (property.CanRead)
+                {
+                    try
+                    {
+                        //only reflect root properties.
+                        dictionary.Add(property.Name, property.GetValue(source, null));
+                    }
+                    catch(Exception ex)
+                    {
+                        dictionary.Add("ERROR_" + property.Name, ex.Message);
+                    }
+                }
+            }
+
+            return dictionary;
+        }
+
+    }
+}
---
+++ src/StringBuilderPool.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Extensions
+{
+    /// <summary>
+    /// Provides a cached reusable instance of a StringBuilder per thread.
+    /// </summary>
+    ///<remarks>
+    ///will reduce overall GC for small strings
+    ///</remarks>
+    public static class StringBuilderPool
+    {
+        //Avoid stringbuilder block fragmentation within a thread context.
+        private const int MAX_BUILDER_SIZE = 360;
+        //default capacity // == StringBuilder.DefaultCapacity
+        private const int DEFAULT_CAPACITY = 16;
+
+        [ThreadStatic]
+        private static StringBuilder s_cachedInstance;
+
+        /// <summary>
+        /// Gets a string builder to use of a particular size.
+        /// </summary>
+        public static StringBuilder Get(int capacity = DEFAULT_CAPACITY)
+        {
+            if (capacity <= MAX_BUILDER_SIZE)
+            {
+                StringBuilder sb = s_cachedInstance;
+                if (sb != null)
+                {
+                    // Avoid stringbuilder block fragmentation by getting a new StringBuilder
+                    // when the requested size is larger than the current capacity
+                    if (capacity <= sb.Capacity)
+                    {
+                        s_cachedInstance = null;
+                        //clear buffer
+                        sb.Clear();
+                        return sb;
+                    }
+                }
+            }
+            return new StringBuilder(capacity);
+        }
+
+        /// <summary>
+        /// Place the specified builder in the cache if it is not too big.
+        /// </summary>
+        public static void Release(this StringBuilder sb)
+        {
+            if (sb?.Capacity <= MAX_BUILDER_SIZE)
+            {
+                s_cachedInstance = sb;
+            }
+        }
+
+        /// <summary>
+        /// Gets the resulting string and releases a StringBuilder instance.
+        /// </summary>
+        public static string GetStringAndRelease(this StringBuilder sb)
+        {
+            string result = sb?.ToString();
+            Release(sb);
+            return result;
+        }
+    }
+}
